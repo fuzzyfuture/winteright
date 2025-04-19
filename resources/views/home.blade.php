c1faee4da7b57d65017d3ade32b154996cd37a06
nate <nathangchay@gmail.com>
2025-04-19 00:06:14 -0600

    Add recently ranked view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,4 +23,22 @@
         <h3>Why "winteright"?</h3>
         <p>I was playing <a href="https://osu.ppy.sh/beatmapsets/1281337#osu/2661429" target="_blank">this map</a> when I decided I want to actually start this project.</p>
     </div>
+    <h3>Recently ranked</h3>
+    @if ($lastSynced)
+        <p class="text-muted">Last updated: {{ \Carbon\Carbon::parse($lastSynced)->diffForHumans() }}</p>
+    @endif
+    <ul class="list-group mb-4">
+        @foreach ($recentlyRanked as $set)
+            <a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+               href="{{ url("/mapsets/$set->set_id") }}">
+                <div>
+                    <strong>{{ $set->artist }} - {{ $set->title }}</strong><br>
+                    <small class="text-muted">
+                        {{ $set->beatmaps_count }} difficult{{ $set->beatmaps_count !== 1 ? 'ies' : 'y' }}
+                    </small>
+                </div>
+                <span class="text-muted">{{ $set->date_ranked?->format('Y-m-d') }}</span>
+            </a>
+        @endforeach
+    </ul>
 @endsection
