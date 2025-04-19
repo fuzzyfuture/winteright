8f4ad403309154286238d8291e48c01c00111b40
nate <nathangchay@gmail.com>
2025-04-19 00:06:27 -0600

    Add recent beatmaps and timestamp for home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\BeatmapService;
+use App\Services\SiteInfoService;
 use App\Services\StatsService;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     protected StatsService $statsService;
+    protected BeatmapService $beatmapService;
+    protected SiteInfoService $siteInfoService;
 
-    public function __construct(StatsService $statsService)
+    public function __construct(StatsService $statsService, BeatmapService $beatmapService, SiteInfoService $siteInfoService)
     {
         $this->statsService = $statsService;
+        $this->beatmapService = $beatmapService;
+        $this->siteInfoService = $siteInfoService;
     }
 
     public function index()
     {
         $user = Auth::user();
         $stats = $this->statsService->getHomePageStats();
+        $recentlyRanked = $this->beatmapService->getRecentBeatmaps();
+        $lastSynced = $this->siteInfoService->getLastSyncedRankedBeatmaps();
 
-        return view('home', compact('user', 'stats'));
+        return view('home', compact('user', 'stats', 'recentlyRanked', 'lastSynced'));
     }
 }
