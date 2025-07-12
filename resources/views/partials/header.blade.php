0a582f8e280f42e96c8e6fe6112afe896514c669
nate <nathangchay@gmail.com>
2025-07-11 20:05:28 -0600

    Update styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0">
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">winteright</a>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+        <button class="navbar-toggler ml-auto px-0" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="mainNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home') }}">home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('charts.index') ? 'active' : '' }}" href="{{ route('charts.index') }}">charts</a>
+                    <a class="nav-link" href="{{ route('charts.index') }}">charts</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">search</a>
@@ -21,21 +16,21 @@
 
             <ul class="navbar-nav ms-auto">
                 @if (Auth::check())
-                    <li class="nav-item d-flex align-items-center py-2">
-                        <a href="{{ url("/users/".Auth::user()->osu_id) }}" class="text-decoration-none">
-                            <img src="{{ Auth::user()->avatar }}" class="rounded-circle me-2" width="30" height="30" alt="Avatar">
-                            <span class="text-body me-3">{{ Auth::user()->name }}</span>
+                    <li class="nav-item">
+                        <a href="{{ url("/users/".Auth::user()->osu_id) }}" class="nav-link">
+                            <img src="{{ Auth::user()->avatar }}" class="me-1" width="16" height="16" alt="Avatar">
+                            <span class="text-body">{{ Auth::user()->name }}</span>
                         </a>
                     </li>
-                    <li class="nav-item py-2">
+                    <li class="nav-item">
                         <form action="{{ route('auth.osu.logout') }}" method="POST">
                             @csrf
-                            <button type="submit" class="btn btn-sm btn-outline-light">logout</button>
+                            <button type="submit" class="nav-link w-100 text-start">logout</button>
                         </form>
                     </li>
                 @else
-                    <li class="nav-item py-2">
-                        <a href="{{ route('auth.osu.login') }}" class="btn btn-sm btn-outline-light">login with osu!</a>
+                    <li class="nav-item">
+                        <a href="{{ route('auth.osu.login') }}" class="nav-link" >login with osu!</a>
                     </li>
                 @endif
             </ul>
