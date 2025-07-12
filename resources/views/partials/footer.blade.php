b07d2014d1141e862f4b980afa38b60f7f686390
nate <nathangchay@gmail.com>
2025-07-11 20:05:18 -0600

    Add footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,8 @@
-<footer class="bg-dark text-light py-3 mt-auto">
+<footer class="py-3 mt-auto">
     <div class="container text-center">
-        <small>winteright |
-            built by <a href="https://osu.ppy.sh/users/2966685" target="_blank">natetheneet</a> |
-            concept and initial data from <a href="https://github.com/apollo-dw/omdb" target="_blank">omdb</a> by <a href="https://osu.ppy.sh/users/9558549" target="_blank">apollo</a></small>
+        winteright 2025 |
+        <a href="https://github.com/fuzzyfuture/winteright" target="_blank">github</a> |
+        built by <a href="https://osu.ppy.sh/users/2966685" target="_blank">natetheneet</a> |
+        concept and initial data from <a href="https://github.com/apollo-dw/omdb" target="_blank">omdb</a> by <a href="https://osu.ppy.sh/users/9558549" target="_blank">apollo</a>
     </div>
 </footer>
