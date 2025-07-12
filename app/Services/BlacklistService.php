aebc61c9497ef8b7b89b67cc315b3bc1ca6a036d
nate <nathangchay@gmail.com>
2025-07-11 20:04:53 -0600

    Implement `GetBlacklist`

--- app/Services/BlacklistService.php
+++ app/Services/BlacklistService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class BlacklistService
@@ -14,5 +15,16 @@ class BlacklistService
     public function IsBlacklisted(int $osuId): bool
     {
         return DB::table('blacklist')->where('osu_id', $osuId)->exists();
+    }
+
+    /**
+     * Returns the blacklist as an array of osu! user IDs.
+     * @return array The array of osu! user IDs in the blacklist.
+     */
+    public function GetBlacklist(): array
+    {
+        return Cache::remember('blacklist', 3600, function() {
+            return DB::table('blacklist')->pluck('osu_id')->toArray();
+        });
     }
 }
