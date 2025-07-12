4da35fca57da33216e4a4f0fe0a7df673ee92161
nate <nathangchay@gmail.com>
2025-07-11 22:41:16 -0600

    Update styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,34 +21,39 @@
         <h3>why "winteright"?</h3>
         <p>I was playing <a href="https://osu.ppy.sh/beatmapsets/1281337#osu/2661429" target="_blank">this map</a> when I decided I want to actually start this project.</p>
     </div>
-    <h3 class="mb-1">recently ranked</h3>
-    @if ($lastSynced)
-        <small class="text-muted d-block mb-3">last updated: {{ \Carbon\Carbon::parse($lastSynced)->diffForHumans() }}</small>
-    @endif
-    <ul class="list-group">
-        @foreach ($recentlyRanked as $set)
-            <div class="list-group-item d-flex flex-column align-items-start flex-md-row align-items-md-center p-2">
-                <div class="beatmap-list-img">
-                    <img class="img-fluid" src="https://assets.ppy.sh/beatmaps/{{ $set->set_id }}/covers/cover.jpg" alt="beatmap bg" />
-                </div>
-                <div class="mt-2 mt-md-0 ms-md-3">
-                    <a href="{{ url("/mapsets/$set->set_id") }}"><strong>{{ $set->artist }} - {{ $set->title }}</strong></a>
-                    <a href="https://osu.ppy.sh/beatmapsets/{{ $set->set_id }}#osu"
-                       target="_blank"
-                       rel="noopener noreferrer"
-                       title="view on osu!"
-                       class="opacity-50 small">
-                        <i class="bi bi-box-arrow-up-right"></i>
-                    </a>
-                    <small class="text-muted d-block">
-                        by {{ $set->creator_label }}
-                    </small>
-                    <small class="text-muted d-block">
-                        {{ $set->beatmaps_count }} difficult{{ $set->beatmaps_count !== 1 ? 'ies' : 'y' }}
-                    </small>
-                </div>
-                <span class="text-muted ms-auto">{{ $set->date_ranked?->format('Y-m-d') }}</span>
-            </div>
-        @endforeach
-    </ul>
+    <div class="row">
+        <div class="col-md-6">
+            <h3 class="mb-1">recently ranked</h3>
+            @if ($lastSynced)
+                <small class="text-muted d-block mb-3">last updated: {{ \Carbon\Carbon::parse($lastSynced)->diffForHumans() }}</small>
+            @endif
+            <ul class="list-group">
+                @foreach ($recentlyRanked as $set)
+                    <div class="list-group-item d-flex align-items-center">
+                        <img src="https://assets.ppy.sh/beatmaps/{{ $set->set_id }}/covers/cover.jpg" alt="beatmap bg" width="175" />
+                        <div class="ms-2">
+                            <a href="{{ url("/mapsets/$set->set_id") }}"><strong>{{ $set->artist }} - {{ $set->title }}</strong></a>
+                            <a href="https://osu.ppy.sh/beatmapsets/{{ $set->set_id }}#osu"
+                               target="_blank"
+                               rel="noopener noreferrer"
+                               title="view on osu!"
+                               class="opacity-50 small">
+                                <i class="bi bi-box-arrow-up-right"></i>
+                            </a>
+                            <small class="text-muted d-block">
+                                by {{ $set->creator_label }}
+                            </small>
+                            <small class="text-muted d-block">
+                                {{ $set->beatmaps_count }} difficult{{ $set->beatmaps_count !== 1 ? 'ies' : 'y' }}
+                            </small>
+                        </div>
+                        <span class="text-muted ms-auto">{{ $set->date_ranked?->format('Y-m-d') }}</span>
+                    </div>
+                @endforeach
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h3 class="mb-1">recent ratings</h3>
+        </div>
+    </div>
 @endsection
