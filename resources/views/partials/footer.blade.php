cce8740b23d9d56b8990e3b305ed8d88971dcfd0
nate <nathangchay@gmail.com>
2025-07-23 10:25:24 -0600

    Change footer link to nate

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container text-center">
         winteright 2025 |
         <a href="https://github.com/fuzzyfuture/winteright" target="_blank">github</a> |
-        built by <a href="https://osu.ppy.sh/users/2966685" target="_blank">natetheneet</a> |
+        built by <a href="https://osu.ppy.sh/users/2966685" target="_blank">nate</a> |
         concept and initial data from <a href="https://github.com/apollo-dw/omdb" target="_blank">omdb</a> by <a href="https://osu.ppy.sh/users/9558549" target="_blank">apollo</a>
     </div>
 </footer>
