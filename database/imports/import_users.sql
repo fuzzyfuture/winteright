ec032a16c12e2aaedab6a017fd8dd59bdc3bb4a0
nate <nathangchay@gmail.com>
2025-07-23 12:41:49 -0600

    Use `id` instead of `osu_id`, remove rating_labels insert

--- database/imports/import_users.sql
+++ database/imports/import_users.sql
@@ -1,5 +1,5 @@
 INSERT INTO winteright.users (
-    osu_id, name, banned, weight, hide_ratings,
+    id, name, banned, weight, hide_ratings,
     bio, title, last_seen_at, ip_address,
     created_at, updated_at
 )
@@ -8,17 +8,3 @@ SELECT
     CustomDescription, UserTitle, NULL, NULL,
     NOW(), NOW()
 FROM omdb_old.users;
-
-INSERT INTO winteright.rating_labels (
-    user_id,
-    rating_0_0, rating_0_5, rating_1_0, rating_1_5, rating_2_0,
-    rating_2_5, rating_3_0, rating_3_5, rating_4_0, rating_4_5, rating_5_0,
-    created_at, updated_at
-)
-SELECT
-    u.id,
-    o.Custom00Rating, o.Custom05Rating, o.Custom10Rating, o.Custom15Rating, o.Custom20Rating,
-    o.Custom25Rating, o.Custom30Rating, o.Custom35Rating, o.Custom40Rating, o.Custom45Rating, o.Custom50Rating,
-    NOW(), NOW()
-FROM omdb_old.users o
-         JOIN winteright.users u ON o.UserID = u.osu_id;
