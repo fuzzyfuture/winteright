5a91da11b50064d0deddab0b51a65cc766a3f74b
nate <nathangchay@gmail.com>
2025-07-23 12:40:34 -0600

    Add comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user');
+    }
+}
