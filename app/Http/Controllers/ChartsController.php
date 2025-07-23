8a4f411ce538135444b7693039256eb1e9bc2cac
nate <nathangchay@gmail.com>
2025-07-23 16:46:01 -0600

    Update `index` to use custom paginator

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Services\BeatmapService;
 use App\Services\ChartsService;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
 
 class ChartsController extends Controller
 {
@@ -16,10 +19,37 @@ class ChartsController extends Controller
         $this->beatmapService = $beatmapService;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $topBeatmaps = $this->chartsService->getTopAllTime();
+        $page = $request->get('page', 1);
+        $year = $request->query('year');
+        $excludeRated = $request->query('excludeRated');
+
+        $perPage = 50;
+        $maxPages = 200;
+        $maxResults = $perPage * $maxPages;
+
+        $offset = ($page - 1) * $perPage;
+
+        $actualCount = $this->chartsService->getTopBeatmapsCount($year, $excludeRated, Auth::user());
+        $totalResults = min($actualCount, $maxResults);
+
+        $beatmapData = $this->chartsService->getTopBeatmaps($year, $excludeRated, Auth::user(), $offset, $perPage);
+
+        $topBeatmaps = new LengthAwarePaginator(
+            $beatmapData,
+            $totalResults,
+            $perPage,
+            $page,
+            ['path' => $request->url()]
+        );
+
+        $topBeatmaps->appends($request->query());
+
         $this->beatmapService->applyCreatorLabels($topBeatmaps->getCollection());
-        return view('charts.index', compact('topBeatmaps'));
+        $beatmapYears = $this->beatmapService->getBeatmapYears();
+
+        return view('charts.index', compact('topBeatmaps', 'beatmapYears', 'year',
+            'excludeRated'));
     }
 }
