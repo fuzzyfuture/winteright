50041e55733af2ce8ffb9927bb92d60f8b57c526
nate <nathangchay@gmail.com>
2025-08-01 13:01:38 -0600

    Implement `getName` and `getIdByName`

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserService
 {
@@ -40,5 +41,45 @@ class UserService
             ->pluck('count', 'rating_bin');
 
         return compact('user', 'ratingSpread', 'recentRatings');
+    }
+
+    /**
+     * Retrieves a user's name from their ID. Uses the beatmap creator names table as a fallback, if the user is not
+     * a winteright user.
+     * @param int $id The user's ID.
+     * @return string The user's name.
+     */
+    public function getName(int $id): string
+    {
+        $user = User::whereId($id)->first();
+
+        if ($user) {
+            return $user->name;
+        }
+
+        return DB::table('beatmap_creator_names')->where('id', $id)->value('name');
+    }
+
+    /**
+     * Retrieves a user's ID from their name. Uses the beatmap creator names table as a fallback, if the user is not
+     * a winteright user.
+     * @param string $name The user's name.
+     * @return int The user's ID, or -1 if the user is not a winteright user or in the beatmap creator names table.
+     */
+    public function getIdByName(string $name): int
+    {
+        $user = User::whereName($name)->first();
+
+        if ($user) {
+            return $user->id;
+        }
+
+        $creatorId = DB::table('beatmap_creator_names')->where('name', $name)->value('id');
+
+        if ($creatorId) {
+            return $creatorId;
+        }
+
+        return -1;
     }
 }
