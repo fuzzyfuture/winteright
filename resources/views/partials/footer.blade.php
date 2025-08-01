9595fa2a005ca27fc9ce21dd8f3181a4ed3b52b7
nate <nathangchay@gmail.com>
2025-08-01 12:58:02 -0600

    Add ko-fi link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,7 @@
     <div class="container text-center">
         winteright 2025 |
         <a href="https://github.com/fuzzyfuture/winteright" target="_blank">github</a> |
+        <a href="https://ko-fi.com/fuzzyfuture" target="_blank">ko-fi</a> |
         built by <a href="https://osu.ppy.sh/users/2966685" target="_blank">nate</a> |
         concept and initial data from <a href="https://github.com/apollo-dw/omdb" target="_blank">omdb</a> by <a href="https://osu.ppy.sh/users/9558549" target="_blank">apollo</a>
     </div>
