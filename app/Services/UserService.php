e70271260d40a8963849177c0322c9b378746fb9
nate <nate@fuzzyfuture.net>
2025-08-02 20:02:45 -0700

    Remove `getProfileData`

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -28,32 +28,6 @@ class UserService
     }
 
     /**
-     * Retrieves user data for the profile page. Includes their 5 latest ratings and an array of counts for each
-     * rating value.
-     * @param User $user The user.
-     * @return array The user's data for the profile page.
-     */
-    public function getProfileData(User $user): array
-    {
-        $recentRatings = $user->ratings()
-            ->with('beatmap.set')
-            ->latest('updated_at')
-            ->take(5)
-            ->get();
-
-        $beatmapService = app(BeatmapService::class);
-        $beatmapService->applyCreatorLabels($recentRatings->pluck('beatmap'));
-
-        $ratingSpread = $user->ratings()
-            ->selectRaw('score as rating_bin, COUNT(*) as count')
-            ->groupBy('rating_bin')
-            ->orderBy('rating_bin')
-            ->pluck('count', 'rating_bin');
-
-        return compact('user', 'ratingSpread', 'recentRatings');
-    }
-
-    /**
      * Retrieves a user's name from their ID. Uses the beatmap creator names table as a fallback, if the user is not
      * a winteright user.
      * @param int $id The user's ID.
