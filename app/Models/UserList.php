a082bd733b2f98c2fa649df351300820c6f90af1
nate <nate@fuzzyfuture.net>
2025-08-02 20:01:51 -0700

    Create user list model

---
+++ app/Models/UserList.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserList extends Model
+{
+    protected $fillable = ['name', 'description'];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
