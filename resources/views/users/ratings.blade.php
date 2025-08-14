4f0cca8f22b13fffc759c0355ff950f1fa385e58
nate <nate@fuzzyfuture.net>
2025-08-14 11:46:26 -0600

    Use url attribute

--- resources/views/users/ratings.blade.php
+++ resources/views/users/ratings.blade.php
@@ -9,14 +9,7 @@
             <div class="list-group-item d-flex align-items-center p-3">
                 <img src="https://assets.ppy.sh/beatmaps/{{ $rating->beatmap->set->id }}/covers/cover.jpg" alt="beatmap bg" width="175" />
                 <div class="ms-3">
-                    <a href="{{ url('/mapsets/'.$rating->beatmap->set->id) }}"><strong>{{ $rating->beatmap->set->artist }} - {{ $rating->beatmap->set->title }} [{{ $rating->beatmap->difficulty_name }}]</strong></a>
-                    <a href="https://osu.ppy.sh/beatmapsets/{{ $rating->beatmap->set->id }}#osu/{{ $rating->beatmap->id }}"
-                       target="_blank"
-                       rel="noopener noreferrer"
-                       title="view on osu!"
-                       class="opacity-50 small">
-                        <i class="bi bi-box-arrow-up-right"></i>
-                    </a>
+                    <strong>{{ $rating->beatmap->url }}</strong>
                     <small class="text-muted d-block">
                         by {{ $rating->beatmap->creator_label }}
                     </small>
