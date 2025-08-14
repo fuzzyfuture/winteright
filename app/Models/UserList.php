fcf2143a06da02309157c9b72e7a597df9e6a8a7
nate <nate@fuzzyfuture.net>
2025-08-14 11:48:56 -0600

    Add list items relation

--- app/Models/UserList.php
+++ app/Models/UserList.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserList extends Model
 {
@@ -12,5 +13,10 @@ class UserList extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(UserListItem::class);
     }
 }
