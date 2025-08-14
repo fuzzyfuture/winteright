2f81f84d9100fa04181456e0165a5bbed46c041f
nate <nate@fuzzyfuture.net>
2025-08-14 11:43:10 -0600

    Add filter UI

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>lists</h1>
+    <h1 class="mb-3">lists</h1>
+    <div class="card mb-4">
+        <div class="card-body">
+            <form href="{{ route('lists.index') }}" method="GET">
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label class="form-label">name contains</label>
+                        <input class="form-control" name="name" value="{{ $name }}">
+                    </div>
+                    <div class="col-md-6">
+                        <label class="form-label">creator name is</label>
+                        <input class="form-control mb-3" name="creatorName" value="{{ $creatorName }}">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary float-end">search</button>
+            </form>
+        </div>
+    </div>
+    <div class="mb-3">
+        <a href="{{ route('lists.new') }}" class="btn btn-sm btn-outline-primary">new</a>
+    </div>
     <div>
         {{ $lists->links() }}
     </div>
