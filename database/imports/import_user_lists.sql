3aad92b6815b832133549c479c472469bd63d31e
nate <nate@fuzzyfuture.net>
2025-08-14 11:42:52 -0600

    Include old list ID

--- database/imports/import_user_lists.sql
+++ database/imports/import_user_lists.sql
@@ -2,5 +2,5 @@ INSERT IGNORE INTO winteright.user_lists (
     id, name, description, user_id, created_at, updated_at
 )
 SELECT
-    NULL, Title, Description, UserId, CreatedAt, UpdatedAt
-FROM omdb_old.lists
+    ListID, Title, Description, UserId, CreatedAt, UpdatedAt
+FROM omdb_old.lists;
