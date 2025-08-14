a2238aa82009922dba6a459579cba910b4403d1a
nate <nate@fuzzyfuture.net>
2025-08-14 11:47:50 -0600

    Add UI for list items

--- resources/views/lists/show.blade.php
+++ resources/views/lists/show.blade.php
@@ -18,7 +18,59 @@
             last updated: {{ $list->updated_at ? $list->updated_at->toFormattedDateString() : 'never' }}
         </small>
     </div>
-    <div>
+    <div class="mb-4">
         {{ $list->description }}
+    </div>
+    <div>
+        {{ $items->links() }}
+    </div>
+    @forelse($items as $item)
+        <div class="row p-0 rounded shadow-sm mb-2 chart-beatmap-card">
+            @if($item->isUser())
+                <div class="col-md-2 p-2">
+                    <div class="chart-beatmap-img w-100 h-100"
+                         style="background-image: url('https://a.ppy.sh/{{ $item->item_id }}');">
+                    </div>
+                </div>
+                <div class="col-md-10 p-3 ps-1">
+                    <div><small class="text-muted">user</small></div>
+                    @if ($item->item)
+                        <h4><a href="{{ route('users.show', $item->item_id) }}">{{ $item->item->name }}</a></h4>
+                    @else
+                        <h4>{{ $item->item_id }}<a href="https://osu.ppy.sh/users/{{ $item->item_id }}"></a></h4>
+                    @endif
+                    <div>{{ $item->description }}</div>
+                </div>
+            @elseif($item->isBeatmap())
+                <div class="col-md-2 p-2">
+                    <div class="chart-beatmap-img w-100 h-100"
+                         style="background-image: url('{{ $item->item->bg_url }}');">
+                    </div>
+                </div>
+                <div class="col-md-10 p-3 ps-1">
+                    <div><small class="text-muted">beatmap</small></div>
+                    <h5 class="mb-1">{{ $item->item->url }}</h5>
+                    <div class="mb-2">mapped by: {{ $item->item->creator_label }}</div>
+                    <div>{{ $item->description }}</div>
+                </div>
+            @elseif($item->isBeatmapSet())
+                <div class="col-md-2 p-2">
+                    <div class="chart-beatmap-img w-100 h-100"
+                         style="background-image: url('{{ $item->item->bg_url }}');">
+                    </div>
+                </div>
+                <div class="col-md-10 p-3 ps-1">
+                    <div><small class="text-muted">beatmap set</small></div>
+                    <h5 class="mb-1">{{ $item->item->url }}</h5>
+                    <div class="mb-2">mapped by: {{ $item->item->creator_label }}</div>
+                    <div>{{ $item->description }}</div>
+                </div>
+            @endif
+        </div>
+    @empty
+        <p>no items found</p>
+    @endforelse
+    <div>
+        {{ $items->links() }}
     </div>
 @endsection
