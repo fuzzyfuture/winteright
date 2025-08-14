cbcf0a9258117a0e006681f28a1ac7d1ab32cb26
nate <nate@fuzzyfuture.net>
2025-08-14 11:49:48 -0600

    Add search functionality for index, item retrieval for show, and empty methods for get and post new

--- app/Http/Controllers/UserListController.php
+++ app/Http/Controllers/UserListController.php
@@ -2,28 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserListItemType;
+use App\Services\BeatmapService;
 use App\Services\UserListService;
+use Illuminate\Http\Request;
 
 class UserListController extends Controller
 {
     protected UserListService $userListService;
+    protected BeatmapService $beatmapService;
 
-    public function __construct(UserListService $userListService)
+    public function __construct(UserListService $userListService, BeatmapService $beatmapService)
     {
         $this->userListService = $userListService;
+        $this->beatmapService = $beatmapService;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $lists = $this->userListService->search();
+        $name = $request->query('name');
+        $creatorName = $request->query('creatorName');
 
-        return view('lists.index', compact('lists'));
+        $lists = $this->userListService->search($name, $creatorName);
+        $lists->appends($request->query());
+
+        return view('lists.index', compact('lists', 'name', 'creatorName'));
     }
 
     public function show($listId)
     {
         $list = $this->userListService->get($listId);
+        $items = $this->userListService->getItems($listId);
 
-        return view('lists.show', compact('list'));
+        $beatmapItems = $items->where('item_type', UserListItemType::BEATMAP);
+        $beatmapSetItems = $items->where('item_type', UserListItemType::BEATMAP_SET);
+
+        $this->beatmapService->applyCreatorLabels($beatmapItems->map->item);
+        $this->beatmapService->applyCreatorLabelsToSets($beatmapSetItems->map->item);
+
+        return view('lists.show', compact('list', 'items'));
+    }
+
+    public function getNew()
+    {
+        return view('lists.new');
+    }
+
+    public function postNew()
+    {
+
     }
 }
