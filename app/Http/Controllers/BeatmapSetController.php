9e532ae830c932a3f121004f5c63b7af8a157ffb
nate <nathangchay@gmail.com>
2025-08-19 18:54:49 -0600

    Add rating options to `show()` instead of calculating in view

--- app/Http/Controllers/BeatmapSetController.php
+++ app/Http/Controllers/BeatmapSetController.php
@@ -34,13 +34,16 @@ class BeatmapSetController extends Controller
 
         $this->beatmapService->applyCreatorLabels($beatmapSet->beatmaps);
 
-        return view('beatmaps.show', compact('beatmapSet', 'ratings'));
+        $ratingOptions = ['' => 'unrated', 0 => '0.0', 1 => '0.5', 2 => '1.0', 3 => '1.5', 4 => '2.0', 5 => '2.5',
+            6 => '3.0', 7 => '3.5', 8 => '4.0', 9 => '4.5', 10 => '5.0'];
+
+        return view('beatmaps.show', compact('beatmapSet', 'ratings', 'ratingOptions'));
     }
 
     public function ratings($setId)
     {
         $beatmapSet = $this->beatmapService->getBeatmapSet($setId);
-        
+
         $beatmapIds = $beatmapSet->beatmaps->pluck('id');
 
         $ratings = $this->ratingService->getForBeatmaps($beatmapIds, 10);
