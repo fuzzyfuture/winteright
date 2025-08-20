c30ff9fa06c25ef92a1038b17e4ffd4673e0b47c
nate <nathangchay@gmail.com>
2025-08-19 18:57:58 -0600

    Lowercase error messages

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -31,18 +31,19 @@ class RatingController extends Controller
             try {
                 $this->ratingService->clear($userId, $beatmapId);
             } catch (Throwable $e) {
-                return back()->withErrors('Error while clearing rating for beatmap '.$beatmapId.': '.$e->getMessage());
+                return back()->withErrors('error while clearing rating for beatmap '.$beatmapId.': '
+                    .$e->getMessage());
             }
 
-            return back()->with('success', 'Rating removed.');
+            return back()->with('success', 'rating removed.');
         }
 
         try {
-            $this->ratingService->set($userId, $beatmapId, $validated['score']);
+            $this->ratingService->set($userId, $beatmapId, $request->score);
         } catch (Throwable $e) {
-            return back()->withErrors('Error while rating '.$beatmapId.': '.$e->getMessage());
+            return back()->withErrors('error while rating '.$beatmapId.': '.$e->getMessage());
         }
 
-        return back()->with('success', 'Rating saved!');
+        return back()->with('success', 'rating saved!');
     }
 }
