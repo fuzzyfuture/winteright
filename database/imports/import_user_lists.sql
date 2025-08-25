81a5a6b6be4f506384b57501782f69aef9be7304
nate <nate@fuzzyfuture.net>
2025-08-24 18:33:05 -0600

    Set is_public to true for legacy lists

--- database/imports/import_user_lists.sql
+++ database/imports/import_user_lists.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO winteright.user_lists (
-    id, name, description, user_id, created_at, updated_at
+    id, name, description, user_id, is_public, created_at, updated_at
 )
 SELECT
-    ListID, Title, Description, UserId, CreatedAt, UpdatedAt
+    ListID, Title, Description, UserId, true, CreatedAt, UpdatedAt
 FROM omdb_old.lists;
