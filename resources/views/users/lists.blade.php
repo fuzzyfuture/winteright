cccd6aa9be398cc90a2c0e96e2eff6c6c3117afb
nate <nathangchay@gmail.com>
2025-08-29 22:50:30 -0600

    Include favorites count on lists

--- resources/views/users/lists.blade.php
+++ resources/views/users/lists.blade.php
@@ -18,7 +18,7 @@
                 <b><a href="{{ route('lists.show', $list->id) }}">{{ $list->name }}</a></b>
                 <div>
                     <small class="text-muted">
-                        {{ $list->items_count }} items | last updated: {{ $list->updated_at?->toFormattedDateString() ?? 'never' }}
+                        {{ $list->items_count }} items | {{ $list->favorites_count }} favs | last updated: {{ $list->updated_at?->toFormattedDateString() ?? 'never' }}
                     </small>
                 </div>
             </div>
