5232f5eecb2254feaf817be9bb1b340d8d6a6081
nate <nathangchay@gmail.com>
2025-08-31 12:05:39 -0600

    Use enabled modes when retrieving ratings for beatmaps

--- app/Http/Controllers/BeatmapSetController.php
+++ app/Http/Controllers/BeatmapSetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\BeatmapService;
 use App\Services\RatingService;
+use Illuminate\Support\Facades\Auth;
 
 class BeatmapSetController extends Controller
 {
@@ -28,7 +29,7 @@ class BeatmapSetController extends Controller
 
         $beatmapIds = $beatmapSet->beatmaps->pluck('id');
 
-        $ratings = $this->ratingService->getForBeatmaps($beatmapIds, 10);
+        $ratings = $this->ratingService->getForBeatmaps($beatmapIds, Auth::user()->enabled_modes ?? 15, 10);
         $ratings->withPath('/mapsets/'.$setId.'/ratings');
 
         $this->beatmapService->applyCreatorLabels($beatmapSet->beatmaps);
