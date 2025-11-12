7b1a8acd67e00d5066deb5aa88b4533e417b40d6
nate <nathangchay@gmail.com>
2025-11-11 22:13:56 -0600

    refactor: use relations for creator labels

--- app/Http/Controllers/BeatmapSetController.php
+++ app/Http/Controllers/BeatmapSetController.php
@@ -32,8 +32,6 @@ class BeatmapSetController extends Controller
         $ratings = $this->ratingService->getForBeatmaps($beatmapIds, Auth::user()->enabled_modes ?? 15, 10);
         $ratings->withPath('/mapsets/'.$setId.'/ratings');
 
-        $this->beatmapService->applyCreatorLabels($beatmapSet->beatmaps);
-
         $ratingOptions = ['' => 'unrated', 0 => '0.0', 1 => '0.5', 2 => '1.0', 3 => '1.5', 4 => '2.0', 5 => '2.5',
             6 => '3.0', 7 => '3.5', 8 => '4.0', 9 => '4.5', 10 => '5.0'];
 
@@ -43,7 +41,6 @@ class BeatmapSetController extends Controller
     public function ratings($setId)
     {
         $beatmapSet = $this->beatmapService->getBeatmapSet($setId);
-
         $beatmapIds = $beatmapSet->beatmaps->pluck('id');
 
         $ratings = $this->ratingService->getForBeatmaps($beatmapIds, Auth::user()->enabled_modes ?? 15, 10);
