15d35909c0ebf149145f160648a5eaaf33b435fa
nate <46760843+fuzzyfuture@users.noreply.github.com>
2025-12-10 13:48:22 -0600

    feat: re-do HTML and add audio preview
    Signed-off-by: nate <46760843+fuzzyfuture@users.noreply.github.com>

--- resources/views/components/beatmaps/beatmap_list_group.blade.php
+++ resources/views/components/beatmaps/beatmap_list_group.blade.php
@@ -1,13 +1,27 @@
-<div class="list-group-item d-flex align-items-center p-2 pe-3">
-    <img src="https://assets.ppy.sh/beatmaps/{{ $map->set->id }}/covers/cover.jpg" alt="beatmap bg" width="175" />
-    <div class="ms-2">
-        <strong>{{ $map->url }}</strong>
-        <small class="text-muted d-block">
-            by {{ $map->creator_label }}
-        </small>
-        <div class="mt-1 d-flex align-items-center gap-2">
-            {{ $map->status_badge }}
-            <small class="text-muted">{{ $map->set->date_ranked->format('Y-m-d') }}</small>
+<div class="list-group-item">
+    <div class="row g-2">
+        <div class="col-md-3">
+            <div class="audio-preview" style="background-image: url({{ $map->bg_url }})"  data-playing="false">
+                <audio src="{{ $map->preview_url }}"></audio>
+                <div class="button-overlay">
+                    <i class="bi bi-play-fill h1 mb-0"></i>
+                </div>
+                <div class="mode-icon-overlay">
+                    {{ $map->mode_icon }}
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div>
+                <strong>{{ $map->url }}</strong>
+                <small class="text-muted d-block">
+                    by {{ $map->creator_label }}
+                </small>
+                <div class="mt-1 d-flex align-items-center gap-2">
+                    {{ $map->status_badge }}
+                    <small class="text-muted">{{ $map->set->date_ranked->format('Y-m-d') }}</small>
+                </div>
+            </div>
         </div>
     </div>
 </div>
