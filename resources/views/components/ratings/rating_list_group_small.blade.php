d035c2418207130af5d9577eaf635611b740099b
nate <46760843+fuzzyfuture@users.noreply.github.com>
2026-01-04 16:53:43 -0600

    refactor: move rating list group items to component
    Signed-off-by: nate <46760843+fuzzyfuture@users.noreply.github.com>

---
+++ resources/views/components/ratings/rating_list_group_small.blade.php
@@ -0,0 +1,15 @@
+<div class="list-group-item d-flex align-items-center ps-1 pe-2">
+    <a href="{{ url("/users/".$rating->user->id) }}"
+       class="d-flex align-items-start flex-nowrap ms-1">
+        <img src="{{ $rating->user->avatar_url }}" width="16" height="16" alt="Avatar">
+        <small class="ms-2">{{ $rating->user->name }}</small>
+    </a>
+    <small class="ms-1">rated</small>
+    <a href="{{ route('beatmaps.show', $rating->beatmap->set->id) }}" class="ms-1 d-flex">
+        <small>{{ $rating->beatmap->set->title }} [{{ $rating->beatmap->difficulty_name }}]</small>
+    </a>
+    <small class="ms-auto text-nowrap text-muted" title="{{ $rating->updated_at }}">
+        {{ $rating->updated_at->diffForHumans() }}
+    </small>
+    <span class="ms-2 badge bg-main fs-6"><small>{{ number_format($rating->score / 2, 1) }}</small></span>
+</div>
