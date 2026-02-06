511c3a2a09dde0376ab6655a6ce1a19f29001710
nate <46760843+fuzzyfuture@users.noreply.github.com>
2026-02-05 20:34:23 -0600

    feat: add migration for hide ratings setting
    Signed-off-by: nate <46760843+fuzzyfuture@users.noreply.github.com>

---
+++ database/migrations/2026_02_06_021939_add_hide_comments_setting.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Enums\HideCommentsOption;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('hide_comments')
+                ->after('hide_ratings')
+                ->nullable(false)
+                ->default(HideCommentsOption::NONE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('hide_comments');
+        });
+    }
+};
