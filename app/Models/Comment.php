60435b5ed2acad49747d824eed670a61a58af816
nate <46760843+fuzzyfuture@users.noreply.github.com>
2026-02-05 20:35:03 -0600

    fix: incorrect foreign key on user relation
    Signed-off-by: nate <46760843+fuzzyfuture@users.noreply.github.com>

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -14,7 +14,7 @@ class Comment extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function beatmapSet(): BelongsTo
