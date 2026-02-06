32c59922121125aea39fd65aff24791ecd538c81
nate <46760843+fuzzyfuture@users.noreply.github.com>
2026-02-05 20:34:42 -0600

    feat: retrieve comments
    Signed-off-by: nate <46760843+fuzzyfuture@users.noreply.github.com>

--- app/Http/Controllers/BeatmapSetController.php
+++ app/Http/Controllers/BeatmapSetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\BeatmapService;
+use App\Services\CommentService;
 use App\Services\RatingService;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,11 +11,13 @@ class BeatmapSetController extends Controller
 {
     protected BeatmapService $beatmapService;
     protected RatingService $ratingService;
+    protected CommentService $commentService;
 
-    public function __construct(BeatmapService $beatmapService, RatingService $ratingService)
+    public function __construct(BeatmapService $beatmapService, RatingService $ratingService, CommentService $commentService)
     {
         $this->beatmapService = $beatmapService;
         $this->ratingService = $ratingService;
+        $this->commentService = $commentService;
     }
 
     public function show($setId)
@@ -32,10 +35,12 @@ class BeatmapSetController extends Controller
         $ratings = $this->ratingService->getForBeatmaps($beatmapIds, Auth::user()->enabled_modes ?? 15, 10);
         $ratings->withPath('/mapsets/'.$setId.'/ratings');
 
+        $comments = $this->commentService->getAllForBeatmapSet($setId);
+
         $ratingOptions = ['' => 'unrated', 0 => '0.0', 1 => '0.5', 2 => '1.0', 3 => '1.5', 4 => '2.0', 5 => '2.5',
             6 => '3.0', 7 => '3.5', 8 => '4.0', 9 => '4.5', 10 => '5.0'];
 
-        return view('beatmaps.show', compact('beatmapSet', 'ratings', 'ratingOptions'));
+        return view('beatmaps.show', compact('beatmapSet', 'ratings', 'ratingOptions', 'comments'));
     }
 
     public function ratings($setId)
